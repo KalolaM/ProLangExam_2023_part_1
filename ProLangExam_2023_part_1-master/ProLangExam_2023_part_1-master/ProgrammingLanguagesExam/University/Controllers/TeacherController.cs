4df4fcefe4fdeaa4fe1405606b217ac6ea2356b6
KalolaM <70270442+KalolaM@users.noreply.github.com>
2023-06-16 16:44:43 +0200

    Add files via upload

---
+++ ProLangExam_2023_part_1-master/ProLangExam_2023_part_1-master/ProgrammingLanguagesExam/University/Controllers/TeacherController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using University.DataAccess;
+
+namespace University.Controllers;
+
+[Route("{controller}")]
+public class TestController
+{
+    private readonly UniversityDbContext _universityDbContext;
+
+    public TestController(UniversityDbContext universityDbContext) => _universityDbContext = universityDbContext;
+
+    [HttpGet]
+    public string Test(string message) => $"Test: database contains {_universityDbContext.Teachers.Count()} teachers with {_universityDbContext.Skills.Count()} skills in total";
+    [HttpGet("All")]
+    public IActionResult GetAllTeachers()
+    {
+        
+        List<Teacher> teachers = GetTeachersFromDataSource();
+
+       
+        List<TeacherWithoutSkills> teachersWithoutSkills = new List<TeacherWithoutSkills>();
+
+     
+        foreach (Teacher teacher in teachers)
+        {
+            TeacherWithoutSkills teacherWithoutSkills = new TeacherWithoutSkills
+            {
+                Id = teacher.Id,
+                Name = teacher.Name,
+                Age = teacher.Age,
+              
+            };
+
+            teachersWithoutSkills.Add(teacherWithoutSkills);
+        }
+
+        return Ok(teachersWithoutSkills);
+    }
+
+
+    private List<Teacher> GetTeachersFromDataSource()
+    {
+       
+        List<Teacher> teachers = new List<Teacher>
+            {
+                new Teacher { Id = 1, Name = "John Doe", Age = 35, Skills = new List<string> { "Math", "Physics" } },
+                new Teacher { Id = 2, Name = "Jane Smith", Age = 42, Skills = new List<string> { "English", "History" } },
+                
+            };
+
+        return teachers;
+    }
+}
+
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<string> Skills { get; set; }
+}
+
+
+public class TeacherWithoutSkills
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+}
+
---
+++ ProLangExam_2023_part_1-master/ProLangExam_2023_part_1-master/ProgrammingLanguagesExam/Utilities/StringUtilities.cs
@@ -0,0 +1,27 @@
+﻿namespace Utilities;
+
+public class StringUtilities
+{
+    public static int ToWords(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return 0;
+
+
+        string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+        HashSet<string> uniqueWords = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+
+        foreach (string word in words)
+        {
+            uniqueWords.Add(word);
+        }
+
+
+        return uniqueWords.Count;
+    }
+}
+
+    }
